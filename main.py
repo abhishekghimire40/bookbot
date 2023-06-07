9131549202ef222969547572edd4c1664bb22373
Abhishek Ghimire <abhishek@pop-os.localdomain>
2023-06-07 21:41:04 +0545

    modified the function to count the number of occurences for each alphabetical character

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def words_count(str):
-    words = file_contents.split()
+def character_count(str):
     count = {}
-    for i in words:
+    for i in str:
+        if not i.isalpha():
+          continue 
         i = i.lower()
         if i in count:
             count[i] += 1
@@ -11,5 +12,14 @@ def words_count(str):
 
 with open('/media/abhishek/Volume3/bookbot/books/frankenstein.txt') as f:
     file_contents = f.read()
-    total_count = words_count(file_contents)
-    print(total_count)
+    total_words = len(file_contents.split())
+    total_count = character_count(file_contents)
+    sorted_count = sorted(total_count.items(),key=lambda item: item[1],reverse=True)
+    print('--- Begin report of books/frankenstein.txt ---')
+    print(f"{total_words} words found in the document\n")
+    for k,v in sorted_count:
+        print(f"The '{k}' character was found {v} times")
+    print(f"--- End Report ---")
+        
+    
+    
