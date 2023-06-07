18b3c781d1e49200faffab0e5190f006a17dc15d
Abhishek Ghimire <abhishek@pop-os.localdomain>
2023-06-07 21:16:35 +0545

    counted occurences of each word in the file

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-    
+def words_count(str):
+    words = file_contents.split()
+    count = {}
+    for i in words:
+        i = i.lower()
+        if i in count:
+            count[i] += 1
+        else:
+            count[i] = 1
+    return count
+
 with open('/media/abhishek/Volume3/bookbot/books/frankenstein.txt') as f:
     file_contents = f.read()
-    words = file_contents.split()
-    print(len(words))
+    total_count = words_count(file_contents)
+    print(total_count)
